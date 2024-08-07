4325a4553f23c35401079b8cddfeff0bcb01cc6e
Alena <al.gnevusheva@gmail.com>
2024-08-07 19:08:02 +0200

    wip: form

---
+++ app/ui/components/form/Form.tsx
@@ -0,0 +1,26 @@
+import React, { HTMLInputTypeAttribute, useActionState } from 'react';
+
+interface Input {
+  name: string;
+  type: HTMLInputTypeAttribute;
+  placeholder: string;
+}
+
+interface FormProps {
+  inputs: Array<Input>;
+  submitHandler: (state: unknown) => void;
+}
+
+const Form = ({ inputs, submitHandler }: FormProps) => {
+  const [error, submitAction, isPending] = useActionState(submitHandler, null);
+
+  return (
+    <form action={submitAction}>
+      <input type="text" name="name" />
+      <button type="submit" disabled={isPending}>
+        Update
+      </button>
+      {error && <p>{error}</p>}
+    </form>
+  );
+};
