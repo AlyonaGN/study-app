15ff74d5b11743da96c6bb5537384eaa4d1b75d2
Alena <al.gnevusheva@gmail.com>
2024-08-08 15:03:38 +0200

    applu tooltips

--- app/ui/components/questionForm/QuestionForm.tsx
+++ app/ui/components/questionForm/QuestionForm.tsx
@@ -3,11 +3,16 @@ import { Heading, HeadingLevel } from '@/app/ui/components/heading/Heading';
 import { NEW_QUESTION_FORM_HEADING } from '@/app/ui/utils/headingsTexts';
 import styles from '@/app/ui/components/questionForm/questionForm.module.css';
 import { NEW_QUESTION_FORM_INPUTS } from '@/app/ui/utils/formTexts';
+import { Tooltip } from '@/app/ui/components/tooltip/Tooltip';
+import { CREATE_NEW_QUESTIONS_TOOLTIP } from '@/app/ui/utils/tooltipTexts';
 
 export const QuestionForm = () => {
   return (
     <div className={styles.container}>
-      <Heading level={HeadingLevel.Two} title={NEW_QUESTION_FORM_HEADING} />
+      <Tooltip text={CREATE_NEW_QUESTIONS_TOOLTIP}>
+        <Heading level={HeadingLevel.Two} title={NEW_QUESTION_FORM_HEADING} />{' '}
+      </Tooltip>
+
       <Form inputs={NEW_QUESTION_FORM_INPUTS} submitHandler={() => null} />
     </div>
   );
--- app/ui/components/questionsSection/QuestionsSection.tsx
+++ app/ui/components/questionsSection/QuestionsSection.tsx
@@ -4,6 +4,8 @@ import { CREATED_QUESTIONS_HEADING } from '@/app/ui/utils/headingsTexts';
 import { QuestionsAndAnswersList } from '@/app/ui/components/questionsList/QuestionsList';
 import { Button, ButtonColor } from '@/app/ui/components/button/Button';
 import { REMOVE_QUESTIONS_BUTTON, SORT_QUESTIONS_BUTTON } from '@/app/ui/utils/buttonTexts';
+import { Tooltip } from '@/app/ui/components/tooltip/Tooltip';
+import { CREATED_QUESTIONS_TOOLTIP } from '@/app/ui/utils/tooltipTexts';
 
 const tempQuestions = [
   { question: 'Did Alena meet all requirements?', answer: 'Yes, she did!', id: '1' },
@@ -30,7 +32,9 @@ const tempQuestions = [
 export const QuestionsSection = () => {
   return (
     <div className={styles.container}>
-      <Heading level={HeadingLevel.Two} title={CREATED_QUESTIONS_HEADING} />
+      <Tooltip text={CREATED_QUESTIONS_TOOLTIP}>
+        <Heading level={HeadingLevel.Two} title={CREATED_QUESTIONS_HEADING} />
+      </Tooltip>
       <QuestionsAndAnswersList questionAnswerPairs={tempQuestions} />
       <div className={styles.buttonsContainer}>
         <Button text={SORT_QUESTIONS_BUTTON} />
