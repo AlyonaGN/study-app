41fdd078f53185ab1b700f9c7f882c8803ec7144
Alena <al.gnevusheva@gmail.com>
2024-08-08 14:17:57 +0200

    components lib: create questions section

--- app/ui/components/questionsSection/QuestionsSection.tsx
+++ app/ui/components/questionsSection/QuestionsSection.tsx
@@ -2,6 +2,8 @@ import styles from '@/app/ui/components/questionsSection/questionsSection.module
 import { Heading, HeadingLevel } from '@/app/ui/components/heading/Heading';
 import { CREATED_QUESTIONS_HEADING } from '@/app/ui/utils/headingsTexts';
 import { QuestionsAndAnswersList } from '@/app/ui/components/questionsList/QuestionsList';
+import { Button, ButtonColor } from '@/app/ui/components/button/Button';
+import { REMOVE_QUESTIONS_BUTTON, SORT_QUESTIONS_BUTTON } from '@/app/ui/utils/buttonTexts';
 
 const tempQuestions = [
   { question: 'Did Alena meet all requirements?', answer: 'Yes, she did!', id: '1' },
@@ -30,6 +32,10 @@ export const QuestionsSection = () => {
     <div className={styles.container}>
       <Heading level={HeadingLevel.Two} title={CREATED_QUESTIONS_HEADING} />
       <QuestionsAndAnswersList questionAnswerPairs={tempQuestions} />
+      <div className={styles.buttonsContainer}>
+        <Button text={SORT_QUESTIONS_BUTTON} />
+        <Button text={REMOVE_QUESTIONS_BUTTON} color={ButtonColor.red} />
+      </div>
     </div>
   );
 };
---
+++ app/ui/utils/tooltipTexts.ts
@@ -0,0 +1,2 @@
+export const CREATED_QUESTIONS_TOOLTIP = 'Here you can find created questions and answers';
+export const CREATE_NEW_QUESTIONS_TOOLTIP = 'Here you can create new questions and their answers';
